624322450744ae6e04b68cf466a97d70cdf5d255
Robert Evans <robermar2@gmail.com>
2021-01-01 16:03:16 -0500

    working

--- kube-check.py
+++ kube-check.py
@@ -2,29 +2,49 @@ import os
 from kubernetes import client, config
 from sense_hat import SenseHat
 
-sense = SenseHat()
+print ("Starting kube-check")
 
-config.load_kube_config()
-v1 = client.CoreV1Api()
+try:
+    sense = SenseHat()
+    sense.clear()
+except Exception as e:
+    print(e)
 
+try:
+    config.load_kube_config(config_file="/home/pi/.kube/admin.conf")
+except Exception as e:
+    print(e)
 
-row = 0
-col = 0
-red = (255, 0, 0)
-green = (0, 255, 0)
+try:
+    v1 = client.CoreV1Api()
 
-#iterate over nodes and get status of each
-response = v1.list_node()
-for node in response.items:
-    #iterate
-    for cond in node.status.conditions:
-        if cond.type == "Ready":
-            if cond.status == "True":
-                sense.set_pixel(row, col, green)
+    row = 0
+    col = 0
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    #iterate over nodes and get status of each
+    response = v1.list_node()
+    for node in response.items:
+        #iterate
+        col = 0
+        for cond in node.status.conditions:
+            if cond.type == "Ready":
+                if cond.status == "True":
+                    sense.set_pixel(col, row, green)
+                else:
+                    sense.set_pixel(col, row, red)
             else:
-                sense.set_pixel(row, col, red)
-        else:
-            if cond.status == "True":
-                sense.set_pixel(row, col, red)
-            else:
-                sense.set_pixel(row, col, green)
+                if cond.status == "True":
+                    sense.set_pixel(col, row, red)
+                else:
+                    sense.set_pixel(col,row, green)
+            col = col + 1
+        row = row + 1
+except Exception as e:
+    sense.set_pixel(col,row, blue)
+    print(e)
+finally:
+    print ("Ending kube-check")
+
---
+++ kube-check.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+#set
+
+PATH="/home/pi/.vscode-server/bin/ea3859d4ba2f3e577a159bc91e3074c5d85c0523/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games"
+
+python /home/pi/pi-sense/kube-check.py
